b4fefb292e3066de2062c55fa40ebc58d9974b02
张海峰 <haifeng.zhang8@pactera.com>
2023-02-03 14:55:27 +0800

    feat: 2 JSX转换

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,90 @@
+// 返回结果是ReactElement的宿主结构
+
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
+import {
+	ElementType,
+	Key,
+	Props,
+	ReactElementType,
+	Ref,
+	Type
+} from 'shared/ReactTypes';
+
+const ReactElement = function (
+	type: Type,
+	key: Key,
+	ref: Ref,
+	props: Props
+): ReactElementType {
+	const element = {
+		$$typeof: REACT_ELEMENT_TYPE,
+		type,
+		key,
+		ref,
+		props,
+		__mark: 'zhf'
+	};
+	return element;
+};
+
+export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
+	console.log('jsx', type, config, maybeChildren);
+
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+	const maybeChildrenLength = maybeChildren.length;
+	if (maybeChildrenLength) {
+		// 一个或多个 [child] [child, child, child]
+		if (maybeChildrenLength === 1) {
+			props.children = maybeChildren[0];
+		} else {
+			props.children = maybeChildren;
+		}
+	}
+	return ReactElement(type, key, ref, props);
+};
+
+export const jsxDEV = (type: ElementType, config: any) => {
+	console.log('jsxDEV', type, config);
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+	return ReactElement(type, key, ref, props);
+};
---
+++ react-demo/src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+const jsx = (
+	<div>
+		hello<span>big-react</span>
+	</div>
+);
+console.log('React', React);
+console.log('jsx', jsx);
+
+// const react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_1__ = {};
+// const _jsxFileName = {};
+
+// const jsx = (0, react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxDEV)(
+// 	'div',
+// 	{
+// 		children: [
+// 			'hello',
+// 			/*#__PURE__*/ (0,
+// 			react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxDEV)(
+// 				'span',
+// 				{
+// 					children: 'big-react'
+// 				},
+// 				void 0,
+// 				false,
+// 				{
+// 					fileName: _jsxFileName,
+// 					lineNumber: 4,
+// 					columnNumber: 8
+// 				},
+// 				undefined
+// 			)
+// 		]
+// 	},
+// 	void 0,
+// 	true,
+// 	{
+// 		fileName: _jsxFileName,
+// 		lineNumber: 3,
+// 		columnNumber: 2
+// 	},
+// 	undefined
+// );
