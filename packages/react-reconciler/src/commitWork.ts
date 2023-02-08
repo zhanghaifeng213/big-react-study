8c6a3abd1c41329576f4f5cd3f35b34f13dc6d9d
张海峰 <haifeng.zhang8@pactera.com>
2023-02-08 16:16:33 +0800

    feat: 实现Mutation子阶段

---
+++ packages/react-reconciler/src/commitWork.ts
@@ -0,0 +1,88 @@
+import { appendChildToContainer, Container } from 'hostConfig';
+import { FiberNode, FiberRootNode } from './fiber';
+import { MutationMask, NoFlags, Placement } from './fiberFlags';
+import { HostComponent, HostRoot, HostText } from './worktags';
+
+let nextEffect: FiberNode | null = null;
+export const commitMutationEffects = (finishedWork: FiberNode) => {
+	nextEffect = finishedWork;
+	while (nextEffect !== null) {
+		const child: FiberNode | null = nextEffect.child;
+		if (
+			(nextEffect.subtreeFlags & MutationMask) !== NoFlags &&
+			child !== null
+		) {
+			nextEffect = nextEffect.child;
+		} else {
+			// 向上遍历 DFS
+			up: while (nextEffect !== null) {
+				commitMutationEffectsOnFiber(nextEffect);
+				const sibling: FiberNode | null = nextEffect.sibling;
+				if (sibling !== null) {
+					nextEffect = sibling;
+					break up;
+				}
+				nextEffect = nextEffect.return;
+			}
+		}
+	}
+};
+
+const commitMutationEffectsOnFiber = (finishedWork: FiberNode) => {
+	const flags = finishedWork.flags;
+	if ((flags & Placement) !== NoFlags) {
+		commitPlacement(finishedWork);
+		finishedWork.flags &= ~Placement;
+	}
+	// flags Update
+	// flags ChildDeletion
+};
+
+const commitPlacement = (finishedWork: FiberNode) => {
+	// finishedwork ~~~ DOM
+	if (__DEV__) {
+		console.warn('执行Placement操作', finishedWork);
+	}
+	// Parent DOM
+	const hostParent = getHostParent(finishedWork);
+	// finishedwork ~~~ DOM append parent
+	appendPlacementNodeIntoContainer(finishedWork, hostParent);
+};
+
+function getHostParent(fiber: FiberNode): Container {
+	let parent = fiber.return;
+	while (parent) {
+		const parentTag = parent.tag;
+		// HostComponent HostRoot
+		if (parentTag === HostComponent) {
+			return parent.stateNode as Container;
+		}
+		if (parentTag === HostRoot) {
+			return (parent.stateNode as FiberRootNode).container;
+		}
+		parent = parent.return;
+	}
+	if (__DEV__) {
+		console.warn('未找到host parent');
+	}
+}
+
+function appendPlacementNodeIntoContainer(
+	finishedWork: FiberNode,
+	hostParent: Container
+) {
+	// fiber host
+	if (finishedWork.tag === HostComponent || finishedWork.tag === HostText) {
+		appendChildToContainer(finishedWork.stateNode, hostParent);
+		return;
+	}
+	const child = finishedWork.child;
+	if (child !== null) {
+		appendPlacementNodeIntoContainer(child, hostParent);
+		let sibling = child.sibling;
+		while (sibling !== null) {
+			appendPlacementNodeIntoContainer(sibling, hostParent);
+			sibling = sibling.sibling;
+		}
+	}
+}
