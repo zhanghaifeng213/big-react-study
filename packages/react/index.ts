ced1d45a31e6596400e90411873102165bda29a6
张海峰 <haifeng.zhang8@pactera.com>
2023-02-10 17:44:11 +0800

    feat: 9-2 测试JSX

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -2,7 +2,7 @@ import currentDispatcher, {
 	Dispatcher,
 	resolveDispatcher
 } from './src/currentDispatcher';
-import { jsxDEV } from './src/jsx';
+import { jsx, jsxDEV, isValidElement as isValidElementFn } from './src/jsx';
 // React
 
 export const useState: Dispatcher['useState'] = (initialState: any) => {
@@ -16,7 +16,9 @@ export const __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED = {
 	currentDispatcher
 };
 
-export default {
-	version: '0.0.0',
-	createElement: jsxDEV
-};
+export const version = '0.0.0';
+
+// TODO 根据环境区分使用jsxDEV/jsx
+// export const createElement = jsxDEV;
+export const createElement = jsx;
+export const isValidElement = isValidElementFn;
