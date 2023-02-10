fc557a79cf6faadd36552324fea0abb2cc49332d
张海峰 <haifeng.zhang8@pactera.com>
2023-02-10 15:23:18 +0800

    feat: 8-2 实现useState

--- demos/test-fc/main.tsx
+++ demos/test-fc/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
+console.log(import.meta.hot);
+
 const App = () => {
+	const [num, setNum] = useState(100);
+	window.setNum = setNum;
 	return (
 		<div>
-			<Child />
+			{num}
+			{/* <Child /> */}
 		</div>
 	);
 };
--- packages/react-reconciler/src/fiberHooks.ts
+++ packages/react-reconciler/src/fiberHooks.ts
@@ -1,5 +1,14 @@
+import { Dispatch, Dispatcher } from 'react/src/currentDispatcher';
 import internals from 'shared/internals';
+import { Action } from 'shared/ReactTypes';
 import { FiberNode } from './fiber';
+import {
+	createUpdate,
+	createUpdateQueue,
+	enqueueUpdate,
+	UpdateQueue
+} from './updateQueue';
+import { scheduleUpdateOnFiber } from './workLoop';
 
 let currentlyRenderingFiber: FiberNode | null = null;
 let workInProgressHook: Hook | null = null;
@@ -13,6 +22,7 @@ interface Hook {
 export function renderWidthHooks(wip: FiberNode) {
 	// 赋值操作
 	currentlyRenderingFiber = wip;
+	// 重置
 	wip.memoizedState = null;
 
 	const current = wip.alternate;
@@ -20,7 +30,7 @@ export function renderWidthHooks(wip: FiberNode) {
 		// update
 	} else {
 		// mount
-		currentDispatcher.current;
+		currentDispatcher.current = HooksDispatcherOnMount;
 	}
 
 	const Component = wip.type;
@@ -30,4 +40,60 @@ export function renderWidthHooks(wip: FiberNode) {
 	// 重置操作
 	currentlyRenderingFiber = null;
 	return children;
+}
+
+const HooksDispatcherOnMount: Dispatcher = {
+	useState: mountState
+};
+
+function mountState<State>(
+	initialState: () => State | State
+): [State, Dispatch<State>] {
+	// 找到当前useState对应的Hook数据
+	const hook = mountWorkInProgressHook();
+	let memoizedState;
+	if (initialState instanceof Function) {
+		memoizedState = initialState();
+	} else {
+		memoizedState = initialState;
+	}
+	const queue = createUpdateQueue<State>();
+	hook.updateQueue = queue;
+	hook.memoizedState = memoizedState;
+	// @ts-ignore
+	const dispatch = dispatchSetState.bind(null, currentlyRenderingFiber, queue);
+	queue.dispatch = dispatch;
+	return [memoizedState, dispatch];
+}
+
+function dispatchSetState<State>(
+	fiber: FiberNode,
+	updateQueue: UpdateQueue<State>,
+	action: Action<State>
+) {
+	const update = createUpdate(action);
+	enqueueUpdate(updateQueue, update);
+	scheduleUpdateOnFiber(fiber);
+}
+
+function mountWorkInProgressHook(): Hook {
+	const hook: Hook = {
+		memoizedState: null,
+		updateQueue: null,
+		next: null
+	};
+	if (workInProgressHook === null) {
+		// mount时第一个hook
+		if (currentlyRenderingFiber === null) {
+			throw new Error('请在函数组件内调用hook');
+		} else {
+			workInProgressHook = hook;
+			currentlyRenderingFiber.memoizedState = workInProgressHook;
+		}
+	} else {
+		// mount时后续的hook
+		workInProgressHook.next = hook;
+		workInProgressHook = hook;
+	}
+	return workInProgressHook;
 }
